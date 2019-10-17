a9ad6295fa9b4be3a00db721ba432fda554912c2
Kwan Chen Sheng <e0318555@u.nus.edu>
2019-10-17 20:22:09 +0800

    Delete getSamplePersons from SampleDataUtil.java

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -29,47 +29,7 @@ public class SampleDataUtil {
                     {"19:30 - 20:00", "Charlie", "0", "Selina"},
                     {"20:00 - 20:30", "Selina", "0", "0"},
                     {"20:30 - 21:00", "Natal", "0", "0"}};
-
-    public static Person[] getSamplePersons() {
-        return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                    new Address("Blk 30 Geylang Street 29, #06-40"),
-                    getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                    new Address("Blk 47 Tampines Street 20, #17-35"),
-                    getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                    new Address("Blk 45 Aljunied Street 85, #11-31"),
-                    getTagSet("colleagues"))
-        };
-    }
-
-    public static ReadOnlyAddressBook getSampleAddressBook() {
-        AddressBook sampleAb = new AddressBook();
-        for (Person samplePerson : getSamplePersons()) {
-            sampleAb.addPerson(samplePerson);
-        }
-        return sampleAb;
-    }
-
-    public static List<Schedule> getSampleSchedulesList() {
-        LinkedList<Schedule> sampleSchedulesList = new LinkedList<>();
-        String date = sampleFilledTable[0][0];
-        LinkedList<LinkedList<String>> sampleData = toTwoDimensionalLinkedList(sampleFilledTable);
-        sampleSchedulesList.add(new Schedule(date, sampleData));
-
-        return sampleSchedulesList;
-    }
-
+    
     /**
      * Returns the given two dimensional array of strings as a two dimensional LinkedList of strings.
      */
